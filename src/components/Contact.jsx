fc99adc82e602a7749d938d0bfbc0350f7e18986
ErnestoHKirk <hooghkirk.e@gmail.com>
2022-05-29 16:16:25 -0700

    Update resume links

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -63,7 +63,7 @@ const Contact = () => {
           </a>
 
           <a
-            href="https://drive.google.com/file/d/1W1Ba7towkPGBIGXdZyr-xmqAwL7xgvX-/view?usp=sharing"
+            href="https://drive.google.com/file/d/1gmgFygN_ZB0GAKcFgf2mXiDdrHNYRieP/view?usp=sharing"
             className="flex text-white text-2xl hover:underline"
           >
             <AiOutlineProfile className="mt-1 mr-2" />
