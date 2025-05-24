f555499b9412687ddc953f386e27f7d96b5edfab
ErnestoHKirk <hooghkirk.e@gmail.com>
2025-05-24 15:50:03 -0500

    Update resume link

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -77,7 +77,7 @@ const Contact = () => {
           </a>
 
           <a
-            href="https://drive.google.com/file/d/1WsUJDSGP0s1_KjK4pVM-zzjt_PUt5mu4/view"
+            href="https://drive.google.com/file/d/1EOqSxBeTz726uNpnZUjSOeu18b7FsRD7/view"
             className="flex text-white text-2xl hover:underline"
           >
             <AiOutlineProfile className="mt-1 mr-2" />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -139,7 +139,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
             <a
               className="hover:text-gray-300 flex justify-between items-center w-full text-gray-300"
-              href="https://drive.google.com/file/d/1WsUJDSGP0s1_KjK4pVM-zzjt_PUt5mu4/view"
+              href="https://drive.google.com/file/d/1EOqSxBeTz726uNpnZUjSOeu18b7FsRD7/view"
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
